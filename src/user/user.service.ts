bc7508da29b67fc6a4cbade4a1d2646ad7b52ffe
Thiago <businessthiagorodrigues@gmail.com>
2026-08-18 22:57:54 -0300

    feat: service funcional

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../database/prisma.service';
+import { Prisma, User } from '../generated/prisma/client';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private readonly prisma: PrismaService) {}
+
+
+    async getUserById(id: Prisma.UserWhereUniqueInput): Promise<User | null> {
+        return this.prisma.user.findUnique({
+            where: id,
+        });
+    }
+
+    async getAllUsers(): Promise<User[]> {
+        return this.prisma.user.findMany();
+    }
+
+    async createUser(data: Prisma.UserCreateInput): Promise<User> {
+        return this.prisma.user.create({
+            data,
+        });
+    }
+
+    async updateUser(params: { where: Prisma.UserWhereUniqueInput; data: Prisma.UserUpdateInput }): Promise<User> {
+        const { where, data } = params;
+        return this.prisma.user.update({
+            where,
+            data,
+        });
+    }
+
+    async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+        return this.prisma.user.delete({
+            where,
+        });
+    }
+}
